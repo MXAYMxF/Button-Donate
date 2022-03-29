1016395128b86a81f276123e8ec23ca8e60d3f7b
MXAYMxF <51118162+MXAYMxF@users.noreply.github.com>
2022-03-29 16:10:53 +0400

    Add files via upload

---
+++ btn-donate.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+<head>
+<meta charset="utf-8" />
+<title>DONATE NOW
+حركة لنشر الخير بدلاً من الفيروسات
+MOVEMENT TO SPREAD GOODNESS INSTEAD OF VIRUSES
+
+</title>
+<link rel="stylesheet" href="css/reset_rtl.css" />
+<link rel="stylesheet" href="css/text_rtl.css" />
+<link rel="stylesheet" href="css/960_rtl.css" />
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link rel="stylesheet" href="css/buttons.css">
+<link rel="stylesheet" href="css/prettify.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/btn-donate.css">
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "test"
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+?>
+
+</head>
+<body>
+
+<div class="container_12">
+  <h2>
+ 
+   </h2>
+  <div class="grid_12">
+    <p>
+      <a href="sms://2441/" class="button button-3d-action button-pill button-jumbo button-block">
+Donate now          </a>
+    </p>
+  </div>
+  <!-- end .grid_12 -->
+
+  <!-- end .grid_7 -->
+  <div class="clear"></div>
+  <div class="grid_6">
+              <h1>DONATIONS</h1>
+
+    <p class="ar">
+
+      مع سرعة انتشار الفيروس العالم صار اناني مع الوباء :
+<ul class="ar">
+  
+  <li class="ar">  الاغنياء صارو اكثر غنى و الفقراء اكثر انعزالية و فقر    </li>
+  <li class="ar">كثير من الناس فقدو وظائفهم و مصدر دخلهم   </li>
+  <li class="ar">   الناس اللي تتعمد تخالط و هي مريضة ما فكرت فكبار السن و اصحاب الامراض المزمنة اللي يموتون جراء انتقال العدوى </li>
+  <li class="ar">الناس اللي تكدس الطعام و تشتري و هيه مش محتاجة  </li>
+  <li class="ar">  الطلب زاد على المطاعم و الكماليات في حين انه في فئة من المجتمع تعاني  </li>
+</ul>
+<br>
+
+<p class="ar">
+و لكن مع سرعة انتشار الفيروس <em style="font-size: 14pt; font-weight: normal;">هناك انتشار ايجابي اسرع لفعل الخير و محاربة سلبية الوباء </em>
+#الصدقات_أكثر_من_الاصابات 
+<br/>
+يمكنك بضغطة زر التبرع رقميا للجمعيات الخيرية <a href="https://www.rcuae.ae/donate.aspx">الهلال الاحمر الاماراتي</a>
+
+شاركنا تبرعك 💚 عبر الهاشتاق:
+<a href="http://twitter.com/#الصدقات_أكثر_من_الاصابات ">
+#الصدقات_أكثر_من_الاصابات 
+</a>
+ لنحصل على عدد متبرعين اكثر من عدد الاصابات بكوفيد 
+
+
+    </p>
+<!--     <p> 
+          <a href="sms://2441/" class="button button-3d-action button-pill button-jumbo button-block">
+            تبرع الآن
+          </a>
+        </p> -->
+
+  </div>
+  <!-- end .grid_6 -->
+  <div class="grid_6">
+    <div id="en">
+                  <h1>313,000 CASES</h1>
+
+      <p>
+      With the rapid spread of the virus, the world became selfish with the epidemic:
+
+      </p>
+      <ul id="englist">
+        <li>The rich became richer, and the poor became more isolated and poor</li>
+      <li>Many people lost their jobs and their source of income </li>
+        <li>
+          People who deliberately mix and are sick, I never thought, for the elderly and people with chronic diseases who die from transmission
+        </li>
+        <li>
+          People who accumulate food and buy, and hey, they do not need
+
+        </li>
+        <li>
+          Demand has increased for restaurants and luxuries while in a segment of society is suffering
+
+        </li>
+
+      </ul>
+
+      <p>
+
+      But with the rapid spread of the virus, <em style="font-size: 14pt; font-weight: normal;">there is a faster positive way to spread and do good to overcome the negativity of the epidemic</em>
+      with one click, you can send an SMS to Emirates red crescent OR digitally donate to <a href="https://www.rcuae.ae/donate.aspx/">UAE Red Crescent</a>
+      Share your donation 💚 via the hashtag: <a href="http://twitter.com/#DONATERS_MORE_THAN_CASES"> #DONATERS_MORE_THAN_CASES</a>
+      Let's get more donors than the number of Covid-19 cases
+          </p>
+
+              <p>       * YOUR DONATIONS WILL DIRECTLY BE SENT TO RED CRESCENT UAE
+</p>
+<!--               <p> 
+          <a href="sms://2441/" class="button button-3d-action button-pill button-jumbo button-block">
+          Donate now          </a>
+          </p> -->
+        </div>
+  </div>
+  <!-- end .grid_6 -->
+  <div class="clear"></div>
+   <div class="grid_12">
+
+    <p>
+      <a href="sms://2441/" class="button button-3d-action button-pill button-jumbo button-block">
+تبرع الآن بالأس أم أس
+</a>
+    </p>
+  </div>
+  <!-- end .grid_12 -->
+
+</div>
+<!-- end .container_16 -->
+</body>
+</html>
